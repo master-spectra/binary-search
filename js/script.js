29388c892026e924ee810959af78d8bcc2eb5f2f
Andrey Omelchenko <63547718+master-spectra@users.noreply.github.com>
2021-01-18 09:43:43 +0200

    fix bug

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 "use strict";
 
 const binarySearch = () => {
-    const answer = 3;
+    const answer = 3.14;
 
     let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let middle = Math.floor(array.length / 2);
 
     const check = () => {
-        if (array[middle] === answer) {
+        if (array[middle] === Math.floor(answer)) {
             console.log(`Вы угадали! Ответ: ${array[middle]}`);
-        } else if (array[middle] > answer) {
+        } else if (array[middle] > Math.floor(answer)) {
             console.log("Много!");
 
             array = array.slice(0, middle);
             middle = Math.floor(array.length / 2);
 
             check();
-        } else if (array[middle] < answer) {
+        } else if (array[middle] < Math.floor(answer)) {
             console.log("Мало!");
 
             array = array.slice(middle + 1, array.length);
